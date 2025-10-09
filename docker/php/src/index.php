7c9ce27684246c8f47839508d0f28fc3ee50de5a
Gaetan <gaetan.athor@gmail.com>
2025-10-09 10:52:40 +0200

    modif index.php et ajout config.php

--- docker/php/src/index.php
+++ docker/php/src/index.php
@@ -1,3 +1,12 @@
 <?php
-phpinfo();
+require_once 'config.php';
+
+echo "<h1>Hello World !</h1>";
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+echo "<p>Connexion à la base de donnée réussi !</p>";
 ?>
